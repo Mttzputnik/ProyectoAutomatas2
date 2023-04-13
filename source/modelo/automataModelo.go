89ebd0d17569ba6a4c359391e311d30d8f608408
Mateo F. Calderón <you@example.com>
2023-04-13 18:44:36 -0500

    Inicio proyecto automatas

---
+++ source/modelo/automataModelo.go
@@ -0,0 +1,9 @@
+package automataModelo
+
+type Automata struct {
+    States       []string            `json:"states"`
+    Alphabet     []string            `json:"alphabet"`
+    Transitions  map[string]map[string]string `json:"transitions"`
+    InitialState string              `json:"initial_state"`
+    FinalStates  []string            `json:"final_states"`
+}
